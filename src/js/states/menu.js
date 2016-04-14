f294d99e71636c00cbee796bed5308777a607ced
jonnopon3000 <jonno.allcock@gmail.com>
2016-04-14 02:38:54 +0100

    States for demo app now split into separate files for cleanliness/preference

---
+++ src/js/states/menu.js
@@ -0,0 +1,15 @@
+var MenuState = function() {
+    return new State("menu",
+        function init() {
+            //nothing to see here
+        },
+        function tick() {
+            GAME.textManager.addString("ProtoGL Demo", "center", 45, new Vec2(GAME.width / 2, GAME.height - 100), new Vec4(50, 255, 255, 0.5), 0);
+
+            GAME.textManager.addString("Space to Start", "center", 45, new Vec2(GAME.width / 2, 100), new Vec4(255, 255, 255, 0.5), 0);
+            if (GAME.inputHandler.isKeyDown(KEYCODES.space)) {
+                GAME.switchToState("game");
+            }
+        }
+    );
+};
