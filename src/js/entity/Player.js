262e3e5dc0d011f28c56ff54b672ae1bb7320a2e
optionallychained <6907442+optionallychained@users.noreply.github.com>
2016-04-16 11:21:49 +0100

    Entities now have shape component to define geometry, big change to entity management + rendering to allow rendering of many different shapes

--- src/js/entity/Player.js
+++ src/js/entity/Player.js
@@ -1,10 +1,12 @@
 var Player = function() {
     var entity = new Entity("player");
-    entity.addComponent(new Sprite(0, 0, 0.25, 1)); //TODO: new Shape()
+    // entity.addComponent(new Sprite(0, 0, 0.25, 1)); //TODO: new Shape()
     entity.addComponent(new Transform2D(new Vec2(GAME.width / 2, GAME.height / 2), new Vec2(40, 40), new Vec2(0, 0)));
     entity.addComponent(new AABBCollisionBox(new Vec2(40, 40)));
     entity.addComponent(new Health(3));
     entity.addComponent(new Points());
+    entity.addComponent(new Shape("triangle", new Vec2(40, 40), new Vec2(GAME.width / 2, GAME.height / 2)));
+    entity.addComponent(new FlatColor(new Vec4(255, 0, 255, 1)));
     // multiplier
 
     entity.components.transform2D.maxVelocity = new Vec2(100, 100);
@@ -35,19 +37,23 @@ var Player = function() {
             transform.angle -= degToRad(10);
         }
 
-        if (transform.position.x < 0) {
-            transform.position.x -= transform.lastMoveDelta.x || transform.lastMoveDelta;
-        }
-        else if (transform.position.x + transform.dimensions.x > GAME.width) {
-            transform.position.x -= transform.lastMoveDelta.x || transform.lastMoveDelta;
+        if (inputHandler.isKeyDown(KEYCODES.e)) {
+            this.components.shape = new Shape("square", new Vec2(40, 40), new Vec2(GAME.width / 2, GAME.height / 2));
         }
 
-        if (transform.position.y < 0) {
-            transform.position.y -= transform.lastMoveDelta.y || transform.lastMoveDelta;
-        }
-        else if (transform.position.y + transform.dimensions.y > GAME.height) {
-            transform.position.y -= transform.lastMoveDelta.y || transform.lastMoveDelta;
-        }
+        // if (transform.position.x < 0) {
+        //     transform.position.x -= transform.lastMoveDelta.x || transform.lastMoveDelta;
+        // }
+        // else if (transform.position.x + transform.dimensions.x > GAME.width) {
+        //     transform.position.x -= transform.lastMoveDelta.x || transform.lastMoveDelta;
+        // }
+        //
+        // if (transform.position.y < 0) {
+        //     transform.position.y -= transform.lastMoveDelta.y || transform.lastMoveDelta;
+        // }
+        // else if (transform.position.y + transform.dimensions.y > GAME.height) {
+        //     transform.position.y -= transform.lastMoveDelta.y || transform.lastMoveDelta;
+        // }
     };
 
     entity.onCollision = function(e) {
